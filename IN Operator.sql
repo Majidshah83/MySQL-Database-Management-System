ec28850028c4c67faa90fdfa5c6c7ee69aa9482a
Majidshah83 <shahmajid508@gmail.com>
2022-11-02 12:27:17 +0500

    SELECT name,quantity FROM products WHERE quantity IN(19,30);

---
+++ IN Operator.sql
@@ -0,0 +1,3 @@
+SELECT * FROM products WHERE name IN('apple','HP') AND quantity<=20;
+SELECT name,quantity FROM products WHERE quantity IN(19,30);
+SELECT name,quantity FROM products WHERE quantity IN(30);
