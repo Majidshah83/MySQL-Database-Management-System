116ec2403c4c1ea7bcf90b341d2393b6dc3f1419
Majidshah83 <shahmajid508@gmail.com>
2022-11-15 00:09:13 +0500

    update

--- like.sql
+++ like.sql
@@ -1,1 +1,5 @@
 SELECT * FROM `products` WHERE name LIKE "d%";
+SELECT * FROM `products` WHERE name LIKE "d%" AND quantity IN(30);
+SELECT * FROM `products` WHERE name LIKE "d%l";
+SELECT * FROM `products` WHERE name LIKE "%de%";
+SELECT * FROM `products` WHERE name LIKE "_a%";
