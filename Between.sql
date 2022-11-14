0ff6a4a1708403628d8694eb15ea299546bc17f3
Majidshah83 <shahmajid508@gmail.com>
2022-11-14 23:56:19 +0500

    like operator

--- Between.sql
+++ Between.sql
@@ -1,3 +1,4 @@
 SELECT * FROM products WHERE quantity BETWEEN 19 AND 20;
 SELECT * FROM `products` WHERE quantity NOT BETWEEN 20 AND 30;
-
+SELECT * FROM `products` WHERE name IN('apple') AND quantity BETWEEN 20 AND 30;
+SELECT * FROM `products` WHERE name BETWEEN "a" AND "p";
