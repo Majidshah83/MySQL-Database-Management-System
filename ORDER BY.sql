766020d40f76194648639f8e533355dbc9f681ab
Majidshah83 <shahmajid508@gmail.com>
2022-11-15 15:36:08 +0500

    update

--- ORDER BY.sql
+++ ORDER BY.sql
@@ -2,3 +2,4 @@ SELECT * from products ORDER By name;
 SELECT * from products ORDER By name DESC;
 SELECT * from products WHERE quantity BETWEEN 20 AND 30 ORDER By name DESC;
 SELECT * from products WHERE quantity IN(20) ORDER By name DESC;
+SELECT * FROM `products` ORDER BY name DESC;
