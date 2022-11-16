31b32f4eeabc4857757c678c2d3f698956b9c468
Majidshah83 <shahmajid508@gmail.com>
2022-11-16 16:46:31 +0500

    update

--- inner join.sql
+++ inner join.sql
@@ -1,1 +1,3 @@
 SELECT * FROM users_verify INNER JOIN users ON users_verify.user_id = users.id;
+SELECT * FROM users_verify UV INNER JOIN users u ON UV.user_id=u.id;
+SELECT UV.user_id,u.name,u.email FROM users_verify UV INNER JOIN users u ON UV.user_id=u.id;
